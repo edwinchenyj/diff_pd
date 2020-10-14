12fe158c9c23ed4f22d5ae4e62ad07f492d299b9
Tao Du <taodu@csail.mit.edu>
2020-10-13 22:43:33 -0400

    Added simplified bunnies. [skip ci]

--- python/example/tet_demo.py
+++ python/example/tet_demo.py
@@ -12,5 +12,6 @@ import os
 import numpy as np
 
 if __name__ == '__main__':
-    obj_file_name = Path(root_path) / 'asset' / 'mesh' / 'torus_tri_mesh.obj'
-    tetrahedralize(obj_file_name, visualize=True)
+    obj_file_name = Path(root_path) / 'asset' / 'mesh' / 'bunny_watertight_simplified2.obj'
+    verts, elements = tetrahedralize(obj_file_name, visualize=True)
+    print('Generated {:4d} vertices and {:4d} elements'.format(verts.shape[0], elements.shape[0]))
