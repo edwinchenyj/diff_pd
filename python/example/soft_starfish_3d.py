Fixed a bug in hydro gradients. Visualized soft starfish.

python/example/soft_starfish_3d.py
@@ -13,15 +13,90 @@ from py_diff_pd.common.grad_check import check_gradients
 from py_diff_pd.core.py_diff_pd_core import StdRealVector
 from py_diff_pd.env.soft_starfish_env_3d import SoftStarfishEnv3d
 
+def check_hydro_gradients(deformable, x0):
+    # Sanity check the gradients of the hydrodynamic forces.
+    water_force_weight = np.random.normal(size=dofs)
+    def loss_and_grad(x):
+        q = x[:dofs]
+        v = x[dofs:]
+        water_force = ndarray(env._deformable.PyForwardStateForce(q, v))
+        loss = water_force.dot(water_force_weight)
+        grad_q = StdRealVector(dofs)
+        grad_v = StdRealVector(dofs)
+        deformable.PyBackwardStateForce(q, v, water_force, water_force_weight, grad_q, grad_v)
+        grad = np.zeros(2 * dofs)
+        grad[:dofs] = ndarray(grad_q)
+        grad[dofs:] = ndarray(grad_v)
+        return loss, grad
+
+    eps = 1e-8
+    atol = 1e-4
+    rtol = 1e-2
+    grads_equal = check_gradients(loss_and_grad, x0, eps, rtol=rtol, atol=atol, verbose=True)
+    if not grads_equal:
+        print_error('ForwardStateForce and BackwardStateForce do not match.')
+
+def visualize_hydro(deformable, bin_mesh_file, img_file, q, v):
+    from py_diff_pd.common.renderer import PbrtRenderer
+    from py_diff_pd.common.project_path import root_path
+    from py_diff_pd.common.tet_mesh import TetMesh3d
+    options = {
+        'file_name': img_file,
+        'light_map': 'uffizi-large.exr',
+        'sample': 64,
+        'max_depth': 2,
+        'camera_pos': (0.15, -0.75, 1.4),
+        'camera_lookat': (0, .15, .4)
+    }
+    renderer = PbrtRenderer(options)
+
+    mesh = TetMesh3d()
+    mesh.Initialize(str(bin_mesh_file))
+    transforms = [
+        ('t', (-0.075, -0.075, 0)),
+        ('s', 3),
+        ('t', (0.2, 0.4, 0.2))
+    ]
+    #renderer.add_tri_mesh(mesh, color=(.6, .3, .2),
+    #    transforms=transforms, render_tet_edge=False,
+    #)
+
+    # Render water force.
+    hydro_force = deformable.PyForwardStateForce(q, v)
+    f = np.reshape(ndarray(hydro_force), (-1, 3))
+    q = np.reshape(ndarray(mesh.py_vertices()), (-1, 3))
+    for fi, qi in zip(f, q):
+        scale = 1.0
+        v0 = qi
+        v3 = scale * fi + qi
+        v1 = (2 * v0 + v3) / 3
+        v2 = (v0 + 2 * v3) / 3
+        if np.linalg.norm(fi) == 0: continue
+        renderer.add_shape_mesh({
+                'name': 'curve',
+                'point': ndarray([v0, v1, v2, v3]),
+                'width': 0.001
+            },
+            color=(.2, .6, .3),
+            transforms=transforms
+        )
+
+    renderer.add_tri_mesh(Path(root_path) / 'asset/mesh/curved_ground.obj',
+        texture_img='chkbd_24_0.7', transforms=[('s', 2)])
+
+    renderer.render()
+
 if __name__ == '__main__':
     seed = 42
     np.random.seed(seed)
     folder = Path('soft_starfish_3d')
-    youngs_modulus = 1e6
+    youngs_modulus = 5e5
     poissons_ratio = 0.4
+    act_stiffness = 2e6
     env = SoftStarfishEnv3d(seed, folder, {
         'youngs_modulus': youngs_modulus,
-        'poissons_ratio': poissons_ratio })
+        'poissons_ratio': poissons_ratio,
+        'act_stiffness': act_stiffness })
     deformable = env.deformable()
 
     # Optimization parameters.
@@ -40,11 +115,25 @@ if __name__ == '__main__':
     q0 = env.default_init_position()
     v0 = np.zeros(dofs)
     dt = 1e-2
-    frame_num = 50
-    a0 = [np.zeros(act_dofs) for _ in range(frame_num)]
+    frame_num = 4
+    a0 = [np.random.uniform(low=1.99, high=2.01, size=act_dofs) for _ in range(frame_num)]
     vertex_num = int(dofs // 3)
     f0 = np.zeros((vertex_num, 3))
     f0 = f0.ravel()
     f0 = [f0 for _ in range(frame_num)]
     _, info = env.simulate(dt, frame_num, methods[2], opts[2], q0, v0, a0, f0, require_grad=False,
         vis_folder='random')
+
+    # Uncomment the following lines to check gradients.
+    # q_final = ndarray(info['q'][-1])
+    # v_final = ndarray(info['v'][-1])
+    # x0 = np.concatenate([q_final, v_final])
+    # check_hydro_gradients(deformable, x0)
+
+    # Visualize hydrodynamic forces.
+    create_folder(folder / 'vis')
+    for i in range(frame_num + 1):
+        q = ndarray(info['q'][i])
+        v = ndarray(info['v'][i])
+        visualize_hydro(deformable, folder / 'random/{:04d}.bin'.format(i),
+            folder / 'vis/{:04d}.png'.format(i), q, v)

python/py_diff_pd/common/tet_mesh.py
@@ -248,14 +248,15 @@ import trimesh
 import tetgen
 import pyvista as pv
 
-def tetrahedralize(triangle_mesh_file_name, visualize=False):
+def tetrahedralize(triangle_mesh_file_name, visualize=False, normalize_input=True):
     tri_mesh = trimesh.load(triangle_mesh_file_name)
     assert tri_mesh.is_watertight
-    bbx_offset = np.min(tri_mesh.vertices, axis=0)
-    tri_mesh.vertices -= bbx_offset
-    bbx_extent = ndarray(tri_mesh.bounding_box.extents)
-    tri_mesh.vertices /= np.max(bbx_extent)
-    # Now tri_mesh.vertices is bounded by [0, 1].
+    if normalize_input:
+        bbx_offset = np.min(tri_mesh.vertices, axis=0)
+        tri_mesh.vertices -= bbx_offset
+        bbx_extent = ndarray(tri_mesh.bounding_box.extents)
+        tri_mesh.vertices /= np.max(bbx_extent)
+        # Now tri_mesh.vertices is bounded by [0, 1].
     tmp_file_name = '.tmp.stl'
     tri_mesh.export(tmp_file_name)
     mesh = pv.read(tmp_file_name)
@@ -269,8 +270,9 @@ def tetrahedralize(triangle_mesh_file_name, visualize=False):
 
     if visualize:
         tet_grid = tet.grid
+        bbx_center = 0.5 * (np.min(tri_mesh.vertices, axis=0) + np.max(tri_mesh.vertices, axis=0))
         # Plot half the tet.
-        mask = tet_grid.points[:, 2] < 0.5
+        mask = tet_grid.points[:, 2] < bbx_center[2]
         half_tet = tet_grid.extract_points(mask)
 
         plotter = pv.Plotter()

python/py_diff_pd/env/soft_starfish_env_3d.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from py_diff_pd.env.env_base import EnvBase
 from py_diff_pd.common.common import create_folder, ndarray
-from py_diff_pd.common.tet_mesh import generate_tet_mesh, tet2obj, tetrahedralize
+from py_diff_pd.common.tet_mesh import generate_tet_mesh, tet2obj, tetrahedralize, get_boundary_face
 from py_diff_pd.common.tet_mesh import get_contact_vertex as get_tet_contact_vertex
 from py_diff_pd.core.py_diff_pd_core import TetMesh3d, TetDeformable
 from py_diff_pd.common.renderer import PbrtRenderer
@@ -21,14 +21,15 @@ class SoftStarfishEnv3d(EnvBase):
 
         youngs_modulus = options['youngs_modulus'] if 'youngs_modulus' in options else 1e6
         poissons_ratio = options['poissons_ratio'] if 'poissons_ratio' in options else 0.49
+        act_stiffness = options['act_stiffness'] if 'act_stiffness' in options else 5e5
 
         # Mesh parameters.
         la = youngs_modulus * poissons_ratio / ((1 + poissons_ratio) * (1 - 2 * poissons_ratio))
         mu = youngs_modulus / (2 * (1 + poissons_ratio))
         density = 1e3
         tmp_bin_file_name = '.tmp.bin'
-        obj_file_name = Path(root_path) / 'asset' / 'mesh' / 'starfish.obj'
-        verts, eles = tetrahedralize(obj_file_name)
+        obj_file_name = Path(root_path) / 'asset' / 'mesh' / 'starfish_simplified.obj'
+        verts, eles = tetrahedralize(obj_file_name, normalize_input=False)
         generate_tet_mesh(verts, eles, tmp_bin_file_name)
         mesh = TetMesh3d()
         mesh.Initialize(str(tmp_bin_file_name))
@@ -42,9 +43,55 @@ class SoftStarfishEnv3d(EnvBase):
         deformable.AddPdEnergy('corotated', [2 * mu,], [])
         deformable.AddPdEnergy('volume', [la,], [])
 
+        # Hydrodynamic forces.
+        v_rho = 1e3
+        v_water = ndarray([0, 0, 0])    # Velocity of the water.
+        # Cd_points = (angle, coeff) pairs where angle is normalized to [0, 1].
+        Cd_points = ndarray([[0.0, 0.05], [0.4, 0.05], [0.7, 1.85], [1.0, 2.05]])
+        # Ct_points = (angle, coeff) pairs where angle is normalized to [-1, 1].
+        Ct_points = ndarray([[-1, -0.8], [-0.3, -0.5], [0.3, 0.1], [1, 2.5]])
+        # The current Cd and Ct are similar to Figure 2 in SoftCon.
+        # surface_faces is a list of (v0, v1, v2) where v0, v1, v2 are the vertex indices of the corners of a boundary face.
+        # The order of v0, v1, v2 follows the right-hand rule: if your right hand follows v0 -> v1 -> v2, your thumb will
+        # point to the outward normal.
+        surface_faces = get_boundary_face(mesh)
+        deformable.AddStateForce('hydrodynamics', np.concatenate([[v_rho,], v_water, Cd_points.ravel(), Ct_points.ravel(),
+            ndarray(surface_faces).ravel()]))
+
         # Dirichlet boundary conditions: TODO.
 
-        # Actuators: TODO.
+        # Actuators.
+        # Range of the center of the starfish: -18mm to +18mm.
+        # Width of the limb: -7.5mm to +7.5mm.
+        half_limb_width = 7.5 / 1000
+        half_center_size = 18 / 1000
+        ele_num = mesh.NumOfElements()
+        x_pos_act_eles = []
+        x_neg_act_eles = []
+        y_pos_act_eles = []
+        y_neg_act_eles = []
+        for ei in range(ele_num):
+            v_idx = list(mesh.py_element(ei))
+            v = [ndarray(mesh.py_vertex(i)) for i in v_idx]
+            v = ndarray(v)
+            # Compute the average location.
+            v_com = np.mean(v, axis=0)
+            x, y, z = v_com
+            if np.abs(x) > half_limb_width and np.abs(y) > half_limb_width: continue
+            if z < 0: continue
+            if x < -half_center_size:
+                x_neg_act_eles.append(ei)
+            elif x > half_center_size:
+                x_pos_act_eles.append(ei)
+            elif y < -half_center_size:
+                y_neg_act_eles.append(ei)
+            elif y > half_center_size:
+                y_pos_act_eles.append(ei)
+
+        deformable.AddActuation(act_stiffness, ndarray([1, 0, 0]), x_neg_act_eles)
+        deformable.AddActuation(act_stiffness, ndarray([1, 0, 0]), x_pos_act_eles)
+        deformable.AddActuation(act_stiffness, ndarray([0, 1, 0]), y_neg_act_eles)
+        deformable.AddActuation(act_stiffness, ndarray([0, 1, 0]), y_pos_act_eles)
 
         # Initial states.
         dofs = deformable.dofs()
@@ -81,7 +128,7 @@ class SoftStarfishEnv3d(EnvBase):
             'light_map': 'uffizi-large.exr',
             'sample': self.__spp,
             'max_depth': 2,
-            'camera_pos': (0.15, -1.75, 0.6),
+            'camera_pos': (0.15, -0.75, 1.4),
             'camera_lookat': (0, .15, .4)
         }
         renderer = PbrtRenderer(options)
@@ -90,10 +137,11 @@ class SoftStarfishEnv3d(EnvBase):
         mesh.Initialize(mesh_file)
         renderer.add_tri_mesh(mesh, color=(.6, .3, .2),
             transforms=[
-                ('s', 0.75),
-                ('t', (-0.3, 0, 0.2))
+                ('t', (-0.075, -0.075, 0)),
+                ('s', 3),
+                ('t', (0.2, 0.4, 0.2))
             ],
-            render_tet_edge=True,
+            render_tet_edge=False,
         )
 
         renderer.add_tri_mesh(Path(root_path) / 'asset/mesh/curved_ground.obj',