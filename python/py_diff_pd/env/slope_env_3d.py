092c39927b327f1879e186aab35769361e8796e8
Tao Du <taodu@csail.mit.edu>
2021-05-13 11:07:02 -0400

    Rewrite slope_3d: now it is a duck.

--- python/py_diff_pd/env/slope_env_3d.py
+++ python/py_diff_pd/env/slope_env_3d.py
@@ -2,12 +2,15 @@ import time
 from pathlib import Path
 
 import numpy as np
+import os
 
 from py_diff_pd.env.env_base import EnvBase
-from py_diff_pd.common.common import create_folder, ndarray
-from py_diff_pd.common.hex_mesh import generate_hex_mesh
-from py_diff_pd.common.display import display_hex_mesh, export_gif
-from py_diff_pd.core.py_diff_pd_core import HexMesh3d, HexDeformable
+from py_diff_pd.common.common import create_folder, ndarray, print_info
+from py_diff_pd.common.tet_mesh import tetrahedralize, read_tetgen_file, generate_tet_mesh, tet2obj
+from py_diff_pd.common.tri_mesh import generate_tri_mesh
+from py_diff_pd.common.project_path import root_path
+from py_diff_pd.common.display import export_gif
+from py_diff_pd.core.py_diff_pd_core import TetMesh3d, TetDeformable
 from py_diff_pd.common.renderer import PbrtRenderer
 from py_diff_pd.common.project_path import root_path
 
@@ -21,24 +24,22 @@ class SlopeEnv3d(EnvBase):
         youngs_modulus = 4e6
         poissons_ratio = 0.45
         state_force_parameters = options['state_force_parameters']
-
-        # Mesh parameters.
-        cell_nums = (4, 4, 4)
-        node_nums = (cell_nums[0] + 1, cell_nums[1] + 1, cell_nums[2] + 1)
-        dx = 0.01
-        origin = (0, 0, 0.001)
-        bin_file_name = str(folder / 'mesh.bin')
-        voxels = np.ones(cell_nums)
-        generate_hex_mesh(voxels, dx, origin, bin_file_name)
-        mesh = HexMesh3d()
-        mesh.Initialize(bin_file_name)
-
-        # FEM parameters.
+        slope_degree = float(options['slope_degree'])
+        initial_height = float(options['initial_height'])
         la = youngs_modulus * poissons_ratio / ((1 + poissons_ratio) * (1 - 2 * poissons_ratio))
         mu = youngs_modulus / (2 * (1 + poissons_ratio))
         density = 1e3
-        deformable = HexDeformable()
-        deformable.Initialize(bin_file_name, density, 'none', youngs_modulus, poissons_ratio)
+
+        # Mesh parameters.
+        tmp_bin_file_name = '.tmp.bin'
+        obj_file_name = Path(root_path) / 'asset' / 'mesh' / 'bob.obj'
+        verts, eles = tetrahedralize(obj_file_name, normalize_input=False, options={ 'minratio': 1.1 })
+        generate_tet_mesh(verts, eles, tmp_bin_file_name)
+        mesh = TetMesh3d()
+        mesh.Initialize(str(tmp_bin_file_name))
+        deformable = TetDeformable()
+        deformable.Initialize(tmp_bin_file_name, density, 'none', youngs_modulus, poissons_ratio)
+        os.remove(tmp_bin_file_name)
 
         # External force.
         g = state_force_parameters[:3]
@@ -46,15 +47,10 @@ class SlopeEnv3d(EnvBase):
 
         # Contact.
         kn, kf, mu = state_force_parameters[3:]
-        deformable.AddStateForce('planar_contact', [0, 0, 1, 0, 3, kn, kf, mu])
-        # Use these lines to try the penetration-free contact.
-        '''
-        friction_node_idx = []
-        for i in range(node_nums[0]):
-            for j in range(node_nums[1]):
-                friction_node_idx.append(i * node_nums[1] * node_nums[2] + j * node_nums[2])
-        deformable.SetFrictionalBoundary('planar', [0.0, 0.0, 1.0, 0.0], friction_node_idx)
-        '''
+        # The line equation:
+        c, s = np.cos(np.deg2rad(slope_degree)), np.sin(np.deg2rad(slope_degree))
+        # -s * x + c * z - c * height = 0.
+        deformable.AddStateForce('planar_contact', [-s, 0, c, -c * initial_height, 3, kn, kf, mu])
 
         # Elasticity.
         deformable.AddPdEnergy('corotated', [2 * mu,], [])
@@ -62,9 +58,38 @@ class SlopeEnv3d(EnvBase):
 
         # Initial conditions.
         dofs = deformable.dofs()
-        q0 = ndarray(mesh.py_vertices())
+        q0 = ndarray(mesh.py_vertices()).reshape((-1, 3))
+        # Place q0 to the right location.
+        q0 = q0 @ ndarray([
+            [c, 0, -s],
+            [0, 1, 0],
+            [s, 0, c]
+        ]).T
+        q0 += ndarray([0, 0, initial_height])
+        q0 = q0.ravel()
         v0 = np.zeros(dofs)
         f_ext = np.zeros(dofs)
+
+        # Create a trimesh obstacle.
+        tri_mesh_name = Path(folder) / 'obs.obj'
+        # Plane equation: -s * x + c * z - c * height = 0.
+        verts = ndarray([
+            [(-c - c * initial_height) / s, -1.25, -1],
+            [(-c - c * initial_height) / s, 1.25, -1],
+            [2, -1.25, (2 * s + c * initial_height) / c],
+            [2, 1.25, (2 * s + c * initial_height) / c],
+            [2, -1.25, -1],
+            [2, 1.25, -1]
+        ])
+        eles = [[1, 0, 2],
+            [2, 3, 1],
+            [5, 1, 3],
+            [2, 0, 4],
+            [4, 0, 1],
+            [1, 5, 4],
+            [3, 2, 5],
+            [5, 2, 4]]
+        generate_tri_mesh(verts, eles, tri_mesh_name)
 
         # Data members.
         self._deformable = deformable
@@ -75,8 +100,6 @@ class SlopeEnv3d(EnvBase):
         self._poissons_ratio = poissons_ratio
         self._state_force_parameters = state_force_parameters
         self._stepwise_loss = False
-        self.__loss_q_grad = np.random.normal(size=dofs)
-        self.__loss_v_grad = np.random.normal(size=dofs)
         self.__spp = int(options['spp']) if 'spp' in options else 4
 
     def material_stiffness_differential(self, youngs_modulus, poissons_ratio):
@@ -95,19 +118,26 @@ class SlopeEnv3d(EnvBase):
             'light_map': 'uffizi-large.exr',
             'sample': self.__spp,
             'max_depth': 2,
-            'camera_pos': (0.15, -.1, 0.1),
-            'camera_lookat': (0, .0, .0)
+            'camera_pos': (-0.9, -.9, 0.9),
+            'camera_lookat': (0, 0, 0.1)
         }
         renderer = PbrtRenderer(options)
 
-        mesh = HexMesh3d()
+        mesh = TetMesh3d()
         mesh.Initialize(mesh_file)
-        renderer.add_hex_mesh(mesh, transforms=[('s', 1.)], render_voxel_edge=True, color=[1., .8, .0])
+        vert_num = mesh.NumOfVertices()
+        renderer.add_tri_mesh(mesh, transforms=[('s', 0.1)], render_tet_edge=True, color=[1., .8, .0])
         renderer.add_tri_mesh(Path(root_path) / 'asset/mesh/curved_ground.obj',
-            texture_img='chkbd_24_0.7', transforms=[('s', 1)])
+            transforms=[('s', 200)], color=(.4, .4, .4))
+        renderer.add_tri_mesh(self._folder / 'obs.obj', transforms=[('s', 0.1)], color=(.3, .3, .3))
 
-        renderer.render()
+        renderer.render(light_rgb=(.5, .5, .5))
 
     def _loss_and_grad(self, q, v):
-        loss = q.dot(self.__loss_q_grad) + v.dot(self.__loss_v_grad)
-        return loss, np.copy(self.__loss_q_grad), np.copy(self.__loss_v_grad)
+        q = ndarray(q).copy().reshape((-1, 3))
+        loss = np.mean(q[:, 0])
+        grad_q = np.zeros(q.size).reshape((-1, 3))
+        grad_q[:, 0] = 1 / len(q[:, 0])
+        grad_q = grad_q.ravel()
+        grad_v = np.zeros(v.size)
+        return loss, grad_q, grad_v
