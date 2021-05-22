d4aae127016c06d6c7865d04270a65f41d84e279
Tao Du <taodu@csail.mit.edu>
2021-05-21 22:28:09 -0400

    Final render_slide_3d.

--- python/example/render_slide_3d.py
+++ python/example/render_slide_3d.py
@@ -9,7 +9,11 @@ from py_diff_pd.common.renderer import PbrtRenderer
 from py_diff_pd.common.project_path import root_path
 from py_diff_pd.common.common import ndarray, create_folder
 from py_diff_pd.common.common import print_info, print_ok, print_error
+from py_diff_pd.common.tri_mesh import generate_tri_mesh
 from py_diff_pd.env.slide_env_3d import SlideEnv3d
+from py_diff_pd.common.renderer import PbrtRenderer
+from py_diff_pd.common.project_path import root_path
+from py_diff_pd.core.py_diff_pd_core import TetMesh3d, TetDeformable
 
 if __name__ == '__main__':
     seed = 42
@@ -33,14 +37,15 @@ if __name__ == '__main__':
         'target': target })
     deformable = env.deformable()
 
-
     # Optimization parameters.
     thread_ct = 8
     newton_opt = { 'max_newton_iter': 4000, 'max_ls_iter': 10, 'abs_tol': 1e-9, 'rel_tol': 1e-4, 'verbose': 0, 'thread_ct': thread_ct }
     pd_opt = { 'max_pd_iter': 4000, 'max_ls_iter': 10, 'abs_tol': 1e-9, 'rel_tol': 1e-4, 'verbose': 0, 'thread_ct': thread_ct,
         'use_bfgs': 1, 'bfgs_history_size': 10 }
-    methods = ('pd_eigen', 'newton_pcg', 'newton_cholesky')
-    opts = (pd_opt, newton_opt, newton_opt)
+    # methods = ('pd_eigen', 'newton_pcg', 'newton_cholesky')
+    # opts = (pd_opt, newton_opt, newton_opt)
+    methods = ('pd_eigen',)
+    opts = (pd_opt,)
 
     dt = 5e-3
     frame_num = 200
@@ -71,15 +76,72 @@ if __name__ == '__main__':
             'target': target })
         return env
 
-    # Load data and render.
+    # Load or generate data.
     data = pickle.load(open('slide_3d/data_{:04d}_threads.bin'.format(thread_ct), 'rb'))
     for method, opt in zip(methods, opts):
         create_folder(folder / '{}/init'.format(method), exist_ok=True)
-        x_init = data[method][0]['x']
-        init_env = variable_to_env(x_init)
-        init_env.simulate(dt, frame_num, method, opt, q0, v0, a0, f0, require_grad=False, vis_folder='{}/init'.format(method))
+        if not (folder / method / 'init.data').is_file():
+            x_init = data[method][0]['x']
+            init_env = variable_to_env(x_init)
+            _, info = init_env.simulate(dt, frame_num, method, opt, q0, v0, a0, f0, require_grad=False, vis_folder='{}/init'.format(method))
+            pickle.dump(info, open(folder / method / 'init.data', 'wb'))
 
         create_folder(folder / '{}/final'.format(method), exist_ok=True)
-        x_final = data[method][-1]['x']
-        final_env = variable_to_env(x_final)
-        final_env.simulate(dt, frame_num, method, opt, q0, v0, a0, f0, require_grad=False, vis_folder='{}/final'.format(method))
+        if not (folder / method / 'final.data').is_file():
+            x_final = data[method][-1]['x']
+            final_env = variable_to_env(x_final)
+            _, info = final_env.simulate(dt, frame_num, method, opt, q0, v0, a0, f0, require_grad=False, vis_folder=None)
+            pickle.dump(info, open(folder / method / 'final.data', 'wb'))
+
+    # Render.
+    for method in methods:
+        for name in ['init', 'final']:
+            for i in range(frame_num + 1):
+                img_file = folder / method / name / '{:04d}.png'.format(i)
+                mesh_file = folder / method / name / '{:04d}.bin'.format(i)
+                mesh = TetMesh3d()
+                mesh.Initialize(str(mesh_file))
+
+                options = {
+                    'file_name': img_file,
+                    'light_map': 'uffizi-large.exr',
+                    'sample': 4,
+                    'max_depth': 2,
+                    'camera_pos': (-1.3, -1.2, 1.0),
+                    'camera_lookat': (-0.2, 0.2, -0.1),
+                    'resolution': (1600, 1000),
+                    'fov': 27.7,
+                }
+                renderer = PbrtRenderer(options)
+
+                renderer.add_tri_mesh(mesh, transforms=[('s', 0.1)], render_tet_edge=True, color=[1., .8, .0])
+                renderer.add_tri_mesh(Path(root_path) / 'asset/mesh/curved_ground.obj',
+                    transforms=[('s', 200)], color=(.4, .4, .4))
+                renderer.add_tri_mesh(folder / 'obs.obj', transforms=[('s', 0.1)], color=(.3, .3, .3))
+
+                # Plot the goal location.
+                goal_inner_radius = 1.0
+                goal_outer_radius = 1.2
+                circle_num = 64
+                dc = np.pi * 2 / circle_num
+                verts = []
+                eles = []
+                for i in range(circle_num):
+                    c, s = np.cos(dc * i), np.sin(dc * i)
+                    vi = ndarray([c, s, 0]) * goal_inner_radius
+                    vo = ndarray([c, s, 0]) * goal_outer_radius
+                    verts.append(vi)
+                    verts.append(vo)
+                    ei0 = 2 * i
+                    ei1 = ei0 + 2 if i < circle_num - 1 else 0
+                    eo0 = ei0 + 1
+                    eo1 = ei1 + 1
+                    eles.append((ei0, eo0, eo1))
+                    eles.append((ei0, eo1, ei1))
+
+                verts = ndarray(verts) + target * ndarray([1, 1, 0])
+                eles = np.asarray(eles, dtype=int)
+                generate_tri_mesh(verts, eles, folder / 'target.obj')
+                renderer.add_tri_mesh(folder / 'target.obj', transforms=[('s', 0.1)], color=(.9, .9, .9))
+
+                renderer.render(light_rgb=(.5, .5, .5), verbose=True)
