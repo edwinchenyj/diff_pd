d2640dd762070887fdaa8be6cd9cf029c2214b44
Edwin Chen <edwinchenyj@gmail.com>
2021-10-06 16:27:04 -0700

    add some learning tests

--- cpp/core/src/fem/deformable_sibe_forward.cpp
+++ cpp/core/src/fem/deformable_sibe_forward.cpp
@@ -8,6 +8,51 @@ void Deformable<vertex_dim, element_dim>::ForwardSIBE(const std::string& method,
     const VectorXr& q, const VectorXr& v, const VectorXr& a, const VectorXr& f_ext, const real dt,
     const std::map<std::string, real>& options, VectorXr& q_next, VectorXr& v_next, std::vector<int>& active_contact_idx) const {
         std::cout<<"forward sibe\n";
+
+        CheckError(options.find("max_newton_iter") != options.end(), "Missing option max_newton_iter.");
+        CheckError(options.find("max_ls_iter") != options.end(), "Missing option max_ls_iter.");
+        CheckError(options.find("abs_tol") != options.end(), "Missing option abs_tol.");
+        CheckError(options.find("rel_tol") != options.end(), "Missing option rel_tol.");
+        CheckError(options.find("verbose") != options.end(), "Missing option verbose.");
+        CheckError(options.find("thread_ct") != options.end(), "Missing option thread_ct.");
+        const int max_newton_iter = static_cast<int>(options.at("max_newton_iter"));
+        const int max_ls_iter = static_cast<int>(options.at("max_ls_iter"));
+        const real abs_tol = options.at("abs_tol");
+        const real rel_tol = options.at("rel_tol");
+        const int verbose_level = static_cast<int>(options.at("verbose"));
+        const int thread_ct = static_cast<int>(options.at("thread_ct"));
+        CheckError(max_newton_iter > 0, "Invalid max_newton_iter: " + std::to_string(max_newton_iter));
+        CheckError(max_ls_iter > 0, "Invalid max_ls_iter: " + std::to_string(max_ls_iter));
+
+        omp_set_num_threads(thread_ct);
+
+        const real h = dt;
+        // TODO: this mass is wrong for tri or tet meshes.
+        const real mass = element_volume_ * density_;
+        const real h2m = dt * dt / mass;
+        const VectorXr force = ForwardStateForce(q, v);
+        const real inv_h2m = mass / (h * h);
+        const int max_contact_iter = 5;
+        for (int contact_iter = 0; contact_iter < max_contact_iter; ++contact_iter) {
+            if (verbose_level > 0) std::cout << "Contact iteration " << contact_iter << std::endl;
+            // Fix dirichlet_ + active_contact_nodes.
+            std::map<int, real> augmented_dirichlet = dirichlet_;
+
+            for (const int idx : active_contact_idx) {
+                for (int i = 0; i < vertex_dim; ++i)
+                    augmented_dirichlet[idx * vertex_dim + i] = q(idx * vertex_dim + i);
+            }
+            // Initial guess.
+            VectorXr q_sol = q;
+            VectorXr selected = VectorXr::Ones(dofs_);
+            for (const auto& pair : augmented_dirichlet) {
+                q_sol(pair.first) = pair.second;
+                selected(pair.first) = 0;
+            }
+
+        }
+
+
     }
 
 
--- cpp/tests/main.cxx
+++ cpp/tests/main.cxx
@@ -5,16 +5,86 @@
 #include <fem/tet_deformable.h>
 #include <Eigen/Dense>
 
-TEST_CASE("Initialize deformable with tet mesh"){
+TEST_CASE("Initialize deformable with single tet"){
     Eigen::Matrix<real, 3, 4> undeformed_vertices;
-    undeformed_vertices << 0, 0, 0, 0,
-                        0, 0, 1, 1, 
-                        0, 1, 0, 1;
+    undeformed_vertices <<  0, 0, 0, 1,
+                            0, 0, 1, 0, 
+                            0, 1, 0, 1;
     Eigen::Matrix<int, 4, 1> elements;
     elements << 0,
                 1,
                 2,
                 3;
     TetDeformable tet;
-    tet.Initialize(undeformed_vertices, elements, 1000, "linear", 1e6, 0.45);
+
+    real youngs_modulus = 1e6;
+    real poissons_ratio = 0.45;
+    tet.Initialize(undeformed_vertices, elements, 1000, "linear", youngs_modulus, poissons_ratio);
+    
+    std::map<std::string, real> options;
+    options["max_newton_iter"] = 500;
+    options["max_ls_iter"] = 10;
+    options["abs_tol"] = 1e-9;
+    options["rel_tol"] = 1e-4;
+    options["verbose"] = 2;
+    options["thread_ct"] = 1;
+    VectorXr v(12), f_ext(12), q_next(12), v_next(12);
+    Eigen::Map<VectorXr> q(undeformed_vertices.data(), undeformed_vertices.size());
+    v.setZero();
+    f_ext.setZero();
+    q_next.setZero();
+    v_next.setZero();
+    VectorXr a(0);
+    std::vector<int> contact;
+    real dt = 0.005;
+
+    tet.Forward("newton_pardiso",q,v,a,f_ext,dt,options,q_next,v_next,contact);
+}
+
+TEST_CASE("Initialize deformable with single tet with pd energy and gravity"){
+    Eigen::Matrix<real, 3, 4> undeformed_vertices;
+    undeformed_vertices <<  0, 0, 0, 1,
+                            0, 0, 1, 0, 
+                            0, 1, 0, 1;
+    Eigen::Matrix<int, 4, 1> elements;
+    elements << 0,
+                1,
+                2,
+                3;
+    TetDeformable tet;
+
+    real youngs_modulus = 1e6;
+    real poissons_ratio = 0.45;
+    tet.Initialize(undeformed_vertices, elements, 1000, "linear", youngs_modulus, poissons_ratio);
+    real la = youngs_modulus * poissons_ratio / ((1 + poissons_ratio) * (1 - 2 * poissons_ratio));
+    real mu = youngs_modulus / (2 * (1 + poissons_ratio));
+
+
+    std::vector<real> state_force_params{0,0,-9.81};
+    tet.AddStateForce("gravity",state_force_params);
+    std::vector<real> pd_corotated_stiffness{2*mu};
+    std::vector<real> pd_volume_stiffness{la};
+    std::vector<int> pd_apply_to_all_elements;
+    pd_apply_to_all_elements.clear();
+    tet.AddPdEnergy("corotated",pd_corotated_stiffness,pd_apply_to_all_elements);
+    tet.AddPdEnergy("volume",pd_volume_stiffness,pd_apply_to_all_elements);
+
+    std::map<std::string, real> options;
+    options["max_newton_iter"] = 500;
+    options["max_ls_iter"] = 10;
+    options["abs_tol"] = 1e-9;
+    options["rel_tol"] = 1e-4;
+    options["verbose"] = 2;
+    options["thread_ct"] = 4;
+    VectorXr v(12), f_ext(12), q_next(12), v_next(12);
+    Eigen::Map<VectorXr> q(undeformed_vertices.data(), undeformed_vertices.size());
+    v.setZero();
+    f_ext.setZero();
+    q_next.setZero();
+    v_next.setZero();
+    VectorXr a(0);
+    std::vector<int> contact;
+    real dt = 0.005;
+
+    tet.Forward("sibe",q,v,a,f_ext,dt,options,q_next,v_next,contact);
 }
